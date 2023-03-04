459b711f23f2883096c4c94bb95236d83d218cfb
Isah Jacob <isahjacob0@gmail.com>
2023-03-04 20:14:26 +0100

    js file

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+
+function App() {
+
+  return (
+    <div className="App">
+     <h1>Welcome to react with django</h1>
+    </div>
+  );
+}
+
+export default App;
