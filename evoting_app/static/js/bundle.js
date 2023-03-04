56671c68ceecca734824689ad8f647321919c47a
Isah Jacob <isahjacob0@gmail.com>
2023-03-04 20:16:02 +0100

    react files

---
+++ evoting_app/static/js/bundle.js
@@ -0,0 +1,13 @@
+// React library code
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Application code
+//import App from './components/App';
+import App from '/src/component/App';
+
+// Render the React app
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
