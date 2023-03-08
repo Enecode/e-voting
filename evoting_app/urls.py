cb487321f6758c7465566cbc79bf38d2a71e6e79
Isah Jacob <isahjacob0@gmail.com>
2023-03-08 10:52:49 +0100

    updated the url and added Verification endpoint

--- evoting_app/urls.py
+++ evoting_app/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .viewset import VerificationViewSet, PartyViewSet, VoterViewSet
+from . import viewset
+from .viewset import PartyViewSet, VoterViewSet, VerificationViewSet
 
 app_name = 'evoting_app'
 router = routers.DefaultRouter()
-router.register(r'verification', VerificationViewSet)
-router.register(r'party', PartyViewSet)
-router.register(r'voter', VoterViewSet)
-
+router.register(r'verifications', viewset.VerificationViewSet, 'verification')
+router.register(r'party', viewset.PartyViewSet, 'party')
+router.register(r'voter', viewset.VoterViewSet, 'voter')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('api/verifications/', VerificationViewSet.as_view({'post': 'create'}), name='verifications'),
 ]
